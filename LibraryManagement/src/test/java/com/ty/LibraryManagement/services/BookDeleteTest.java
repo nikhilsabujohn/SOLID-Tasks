40b0a32fd777942514bb06e4cb3f045e84fb521a
nikhil sabu <nikhiljohn003@gmail.com>
2024-04-22 06:37:57 +0530

    updated test cases

--- LibraryManagement/src/test/java/com/ty/LibraryManagement/services/BookDeleteTest.java
+++ LibraryManagement/src/test/java/com/ty/LibraryManagement/services/BookDeleteTest.java
@@ -1,31 +1,54 @@
 package com.ty.LibraryManagement.services;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.ty.LibraryManagement.dao.BooksRepository;
+import com.ty.LibraryManagement.entity.BooksData;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 public class BookDeleteTest {
+
     @Mock
     private BooksRepository booksRepository;
 
     @InjectMocks
-    private BookServices bookServices;
-    
-    
+    private BookServices bookService; 
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testDeleteBook() {
-
         int id = 1;
-        bookServices.deleteBook(id);
-        verify(booksRepository, times(1)).deleteById(id);
+        BooksData book = new BooksData();
+        when(booksRepository.findById(id)).thenReturn(Optional.of(book));
+
+        boolean isDeleted = bookService.deleteBook(id);
+        assertTrue(isDeleted);
+
+        Mockito.verify(booksRepository, Mockito.times(1)).deleteById(id);
     }
 
+    @Test
+    public void testDeleteBookNotFound() {
+        int id = 1;
+        when(booksRepository.findById(id)).thenReturn(Optional.empty());
+
+        boolean isDeleted = bookService.deleteBook(id);
+        assertFalse(isDeleted);
+
+        Mockito.verify(booksRepository, Mockito.times(0)).deleteById(id);
+    }
 }
